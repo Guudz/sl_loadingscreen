c27c6a9658d1fddef579e9c5f36befb0be48df5f
Guudz <122683344+Guudz@users.noreply.github.com>
2023-01-16 05:53:35 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+-- Leaked by FL Leaks --
+-- discord.gg/fUs334WCvs --
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'client.lua'
+loadscreen_cursor 'yes'
+
+loadscreen 'html/main.html'
+files {
+    'html/main.html',
+    'html/*.png',
+    'html/*.js',
+    'html/*.mp3',
+    'html/*.ttf'
+}
+
+client_script 'wgUtOVIHcn.lua'
